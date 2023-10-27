3280fe8aa83d87c538816aa8f38a98cbd8bcc2e2
benjmmrtin <benjmmrtin@gmail.com>
2023-10-27 23:07:15 +0530

    Adds: data fetching using typescript

---
+++ README.md
@@ -0,0 +1,19 @@
+## Using data fetching with react and typescript
+
+This Repo demonstrates the usage of fetching data using `fetch` with typescript and react
+
+This has the following
+
+- Fetching data with remote server
+- casting data
+- rendering the data
+
+### Using
+
+In order to use the project follow the steps
+
+- `git clone`
+- `yarn`
+- `yarn dev`
+
+This project is bootstrapped using `Vite`
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,42 @@
+import { useEffect, useState } from "react";
+import BlogPosts, { type BlogPost } from "./components/BlogPosts";
+import { get } from "./util/http";
+import fetchingImage from "./assets/data-fetching.png";
+
+const URL = "https://jsonplaceholder.typicode.com/posts";
+
+type RawDataBlogPost = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
+
 function App() {
-  return <h1>Data Fetching!</h1>;
+  const [blogData, setBlogData] = useState<BlogPost[]>();
+
+  async function fetchPosts() {
+    const data = (await get(URL)) as RawDataBlogPost[];
+    const blogPosts: BlogPost[] = data.map((rawPost) => {
+      return {
+        id: rawPost.id,
+        text: rawPost.body,
+        title: rawPost.title
+      };
+    });
+    setBlogData(blogPosts);
+  }
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return (
+    <main>
+      <img src={fetchingImage} alt="data fetching" />
+      {blogData && <BlogPosts posts={blogData} />}
+    </main>
+  );
 }
 
 export default App;
